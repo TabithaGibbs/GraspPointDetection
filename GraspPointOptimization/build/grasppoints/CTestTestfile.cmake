4431f866d8c3bfe4b7fd4e47bf1a28a1485b1e7c
TabithaGibbs <tgibbs@wpi.edu>
2020-10-06 17:28:28 -0400

    second restructure

--- GraspPointOptimization/build/grasppoints/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tabby/Documents/595 Robotic Manipulation/GraspPointDetection/GraspPointOptimization/src/grasppoints
-# Build directory: /home/tabby/Documents/595 Robotic Manipulation/GraspPointDetection/GraspPointOptimization/build/grasppoints
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tabby/GraspPointDetection/src
+# Build directory: /home/tabby/GraspPointDetection/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
