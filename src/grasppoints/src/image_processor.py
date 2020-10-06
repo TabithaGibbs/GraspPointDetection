ee913d3b49cfef68c7c466b71807d387d1e5488b
Albert Enyedy <aenyedy56@gmail.com>
2020-10-06 16:03:09 -0400

    OpenCV, sensor_msgs, integrated into CMakeLists.txt and package.xml, with image_processor.py showing examples of import statements for each.

---
+++ src/grasppoints/src/image_processor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+from cv_bridge import CvBridge 
+from cv_bridge import CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+
+def callback(data):
+    rospy.loginfo("image received")
+    cv_image 
+        
+def image_processor():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('image_processor', anonymous=True)
+
+    rospy.Subscriber("image", Image, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+    rospy.loginfo("running")
+
+if __name__ == '__main__':
+    image_processor()
+    
+
