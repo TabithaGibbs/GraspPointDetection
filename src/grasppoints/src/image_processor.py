1b2d7b5663fc8fe7d4122c1efe5ac9bc2b84c5b2
TabithaGibbs <tgibbs@wpi.edu>
2020-10-07 07:25:23 -0400

    rewrite processor node

--- src/grasppoints/src/image_processor.py
+++ src/grasppoints/src/image_processor.py
@@ -7,25 +7,24 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
 
-def callback(data):
-    rospy.loginfo("image received")
+
         
-def image_processor():
+class image_processor:
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('image_processor', anonymous=True)
+    def __init__(self):
+        rospy.init_node('image_processor', anonymous=True)
 
-    rospy.Subscriber('acquired_image', Image, callback)
+        rospy.Subscriber('acquired_image', Image, self.callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-    rospy.loginfo("running")
+        self.bridge = CvBridge()
+
+
+    def callback(self, data):
+        rospy.loginfo("image received")
+        #cv_image = self.bridge.imgmsg_to_
 
 if __name__ == '__main__':
-    image_processor()
+    imageProcessor = image_processor()
+    rospy.spin()
     
 
