d60b87e02e36567fcbababeb3931709dd8bf057b
TabithaGibbs <tgibbs@wpi.edu>
2020-10-06 22:00:43 -0400

    redid png background

--- src/grasppoints/src/ImageAcquirer.py
+++ src/grasppoints/src/ImageAcquirer.py
@@ -20,16 +20,17 @@ class ImageAcquirer:
         rospy.init_node('ImageAcquirer', anonymous = True)
         self.use_camera = rospy.get_param('use camera')
         self.img_pub = rospy.Publisher('acquired_image', Image, queue_size =10)
-        self.rate = rospy.Rate(2000) #2000 hz
+        self.rate = rospy.Rate(10000000000000) #200000 hz
         self.bridge = CvBridge()
 
 
         if self.use_camera:
+            self.vid = cv2.VideoCapture(0)
             while not rospy.is_shutdown():
                 self.pull_camera_image()
                 self.rate.sleep()
+            self.vid.release
         else:
-
             while not rospy.is_shutdown():
                 self.pull_static_image()
                 self.rate.sleep()
@@ -41,10 +42,10 @@ class ImageAcquirer:
         rospack = rospkg.RosPack()
         image_path = rospack.get_path('grasppoints') + '/src/'
         try:
-            cv_image = cv2.imread(image_path + 'calli.png')
+            cv_image = cv2.imread(image_path + 'calli.png', 0)
             image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
             self.img_pub.publish(image_message)
-            rospy.loginfo("Image published")
+            #rospy.loginfo("Image published")
         except IOError:
             rospy.loginfo("Error reading file")
 
@@ -52,6 +53,9 @@ class ImageAcquirer:
         """
         pulls image from webcam and publishes on image topic
         """
+        ret, frame = self.vid.read()
+        cv2.imshow('frame',frame)
+
 
 if __name__ == '__main__':
     node = ImageAcquirer()
